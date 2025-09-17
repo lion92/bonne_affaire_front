b4a3979bfb393f164ee9f1f8b4ab9b5eb9e7f803
lion92 <38532569+lion92@users.noreply.github.com>
2025-09-17 13:36:36 +0200

    lien you tube ou linkedin

--- src/components/lien.js
+++ src/components/lien.js
@@ -1,2 +1,2 @@
-//export default {url: "https://www.krisscode.fr/affaire"}
-export default {url: "http://localhost:3004"}
+export default {url: "https://www.krisscode.fr/affaire"}
+//export default {url: "http://localhost:3004"}
